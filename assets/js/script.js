a4bb997550bd8db9b4c81a8bc11a4d54a5092a75
BurgersOnBrioche <burgersonbrioche@gmail.com>
2017-05-25 00:15:16 -0400

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,19 +40,21 @@ $(document).ready(function() {
         switch (true) {
             case $inputText.length > 0:
                 $("#bagLetter2").css({ display: "none" })
-                $("#letter1").html($inputText.charAt(0).toUpperCase())
+
                 var newLetter1Path = "assets/img/letters/" + $inputText.charAt(0).toUpperCase() + "-" + letter1Choice + ".png"
+                $("#letter1").css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "50%", backgroundPosition: "center" })
                 $("#bagLetter1").attr("src", newLetter1Path) /*css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
                 $("#bagLetter1").css({ display: "block", height: newHeight + "px" })
                     /* $("#bagLetter1").css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })
                      $("#bagLetter2").css({ background: "none" })*/
 
                 if ($inputText.length > 1) {
-                    $("#letter2").html($inputText.charAt(1).toUpperCase())
                     var newLetter2Path = "assets/img/letters/" + $inputText.charAt(1).toUpperCase() + "-" + letter2Choice + ".png"
                     $("#bagLetter2").attr("src", newLetter2Path) /*css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
                     $("#bagLetter2").css({ display: "block", height: newHeight + "px" })
                     $("input").blur()
+                    $("#letter2").css({ background: "url(" + newLetter2Path + ") no-repeat", backgroundSize: "50%", backgroundPosition: "center" })
+
                 }
 
                 /*$("#bagLetter2").css({ background: "url(" + newLetter2Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
@@ -74,13 +76,14 @@ $(document).ready(function() {
             letter1Choice = $(this).data("material-swatch")
             var newLetter1Path = "assets/img/letters/" + $inputText.charAt(0).toUpperCase() + "-" + letter1Choice + ".png"
             $("#bagLetter1").attr("src", newLetter1Path) /*css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
+            $("#letter1").css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "50%", backgroundPosition: "center" })
 
         } else if ($("#letter2").hasClass("active")) {
             letter2Choice = $(this).data("material-swatch")
             var newLetter2Path = "assets/img/letters/" + $inputText.charAt(1).toUpperCase() + "-" + letter2Choice + ".png"
             $("#bagLetter2").attr("src", newLetter2Path) /*css({ background: "url(" + newLetter1Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
-
-            /* $("#bagLetter2").css({ background: "url(" + newLetter2Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
+                /* $("#bagLetter2").css({ background: "url(" + newLetter2Path + ") no-repeat", backgroundSize: "contain", backgroundPosition: "center" })*/
+            $("#letter2").css({ background: "url(" + newLetter2Path + ") no-repeat", backgroundSize: "50%", backgroundPosition: "center" })
 
         }
     })
