c5aa329aab7aa162133d5413a2fa28e42542e499
BurgersOnBrioche <burgersonbrioche@gmail.com>
2017-05-30 16:30:30 -0400

    More tinkering with sizing/ pushing to do deploy to production to see if anything changed with the heights

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,7 @@ var state = {
     width: 12
   },
   letters: 'AA',
+  letterAspectHeight: 0.26,
   materials: ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white'],
   positions: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   activeLetter: 0,
@@ -203,10 +204,11 @@ function updateCustomInfo() {
 function resize() {
   $(".js-tab-cnr").height($(".js-tab-back:not(.js-tab-back-all)").width())
   $(".js-tab-back-all").css({ fontSize: ($(".js-tab-cnr").height() * 0.75) + "px" })
-  $(".js-swatch").height(($(".js-tab-cnr").height() * 1.5) + "px")
+  $(".js-swatch").height(($(".js-palette").height() / 3) + "px")
   $(".palette").css({
     height: "calc(" + ($("#customBarSectionMain").height() - $(".js-tab-cnr ").height() - $(".js-bag-color-thumbs-cnr").height() - $(".js-bag-custom").height() - $("js-letter-label").height()) + "px - 40%)"
   })
+  $(".js-letters").height($(".js-bag-custom").height * state.letterAspectHeight + "px")
 }
 
 
