4425c6e4987d4b88540010dd142d7d4b380a5927
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-05-31 20:38:56 +0100

    Resize on zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,7 +141,7 @@ $(document).on('click', '.js-letter', function(evt) {
   }
 })
 
-//
+// select letter tab
 $(document).on('click', '.js-tab-back', function(evt) {
   var tab = $(this).children('.js-tab')
   if (tab.data('index') > -1) {
@@ -157,14 +157,6 @@ $(document).on('click', '.js-tab-back', function(evt) {
       activeLetter: tab.data('index'),
     })
   }
-})
-
-// stop editing
-$(document).on('click', '.js-close-palette', function(evt) {
-  setState({
-    editing: false,
-    editingBag: false
-  })
 })
 
 function setState(newState) {
@@ -218,6 +210,13 @@ function imgLoadedResize(sender) {
   }
 }
 $(window).on('resize', resize)
+var zoom = window.devicePixelRatio
+setInterval(function() {
+  if( window.devicePixelRatio != zoom ) {
+    resize();
+    zoom = window.devicePixelRatio;
+  }
+}, 300)
 
 function render() {
 
