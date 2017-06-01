cf5ac677407ec317afcdf447edd8dddc0f9fa7c5
BurgersOnBrioche <burgersonbrioche@gmail.com>
2017-05-31 23:12:44 -0400

    New Letters With Darker Black To Match Bag

--- assets/js/script.js
+++ assets/js/script.js
@@ -242,14 +242,12 @@ function setImageLoaded(sender) {
 }
 //resize handler
 function resize() {
-
   $(".js-swatch").css({ maxHeight: ($(".js-swatches").width() / (($(".js-swatch").length - 1)) / 2) + "px" })
   $(".js-tab-back:not(.js-tab-back-all)").width($(".js-tab-cnr").height())
   $(".js-tab-back-all").width($(".js-tab-cnr").height() * 2)
   $(".js-tab-back-all").css({ fontSize: ($(".js-tab-back-all").height() * 0.75) + "px" })
   $(".js-letter-label").height($("js-bag-input").height())
-  $(".js-letters").height(($(".js-bag-custom").height() * state.letterAspectHeight) + "px")
-
+  $(".js-letters").height(($(".js-bag-custom").height() * state.letterAspectHeight) + "px").children("js-letter").width(25)
   $(".fa.fa-arrow-right,.fa.fa-arrow-left").css({ fontSize: $(".js-letter-label").height() + "px" })
   $(".js-bag-color-link").each(function() {
     if ($(this).attr('data-loaded')) {
@@ -258,8 +256,10 @@ function resize() {
   })
 }
 
+
 function imgLoadedResize(sender) {
   $(".js-bag-color-thumb").width()
+
 }
 $(window).on('resize', resize)
 var zoom = window.devicePixelRatio
