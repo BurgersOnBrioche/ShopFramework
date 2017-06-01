282441f161748d62ac513ae20edb3aec52c5c3c2
BurgersOnBrioche <burgersonbrioche@gmail.com>
2017-05-31 23:59:39 -0400

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var tutorial = true
 var letterSpacings = {}
 
 $(document).ready(function() {
-
   // shows arrows for steps
   if (tutorial) {
     $(".fa.fa-arrow-right, .fa.fa-arrow-left").css({ display: "flex" })
@@ -242,6 +241,7 @@ function setImageLoaded(sender) {
 }
 //resize handler
 function resize() {
+  console.log("resize")
   $(".js-swatch").css({ maxHeight: ($(".js-swatches").width() / (($(".js-swatch").length - 1)) / 2) + "px" })
   $(".js-tab-back:not(.js-tab-back-all)").width($(".js-tab-cnr").height())
   $(".js-tab-back-all").width($(".js-tab-cnr").height() * 2)
@@ -249,8 +249,12 @@ function resize() {
   $(".js-letter-label").height($("js-bag-input").height())
   $(".js-letters").height(($(".js-bag-custom").height() * state.letterAspectHeight) + "px").children("js-letter").width(25)
   $(".fa.fa-arrow-right,.fa.fa-arrow-left").css({ fontSize: $(".js-letter-label").height() + "px" })
-  $(".js-letter").each(function() {
-    $(this).width($(this).height * (letterSpacings[state.letters[$(this).data("index")]]))
+
+  $("img.js-letter").each(function() {
+
+    console.log($(this).height() * (letterSpacings[state.letters[$(this).data("index")]]["img"].width / letterSpacings[state.letters[$(this).data("index")]]["img"].height))
+    $(this).width($(this).height() * (letterSpacings[state.letters[$(this).data("index")]]["img"].width / letterSpacings[state.letters[$(this).data("index")]]["img"].height))
+
   })
   $(".js-bag-color-link").each(function() {
     if ($(this).attr('data-loaded')) {
