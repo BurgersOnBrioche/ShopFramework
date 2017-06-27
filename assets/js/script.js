5f3ff66c58a28e79f1c3afc8f130b62c2a3808aa
BurgersOnBrioche <burgersonbrioche@gmail.com>
2017-06-27 13:13:35 -0400

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -568,3 +568,17 @@ function MaterialSelector(props) {
   ].join('')
   return $(html)
 }
+
+function downloadImage() {
+  var node = document.getElementById('customBag');
+
+  domtoimage.toPng(node)
+    .then(function(dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      document.body.appendChild(img);
+    })
+    .catch(function(error) {
+      console.error('oops, something went wrong!', error);
+    });
+}
